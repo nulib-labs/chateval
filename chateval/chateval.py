5d56ae00408121f784c53128cdbebc80833ebbe2
davidschober <davidschob@gmail.com>
2025-03-04 16:26:55 -0600

    added typer

--- chateval/chateval.py
+++ chateval/chateval.py
@@ -1,47 +1,32 @@
-"""chateval
-
-Usage: 
-  chateval [options] <inputcsv> <outputcsv> 
-
-Options:
-
-  -c --with-context   output optional context field
-  -e --evaluate       evaluate the answers
-  -m --model <model>  model to use [default: haiku]
-  -d --debug          show account and model info 
-  -h --help           show this screen
-
-Description:
-
-Takes a set of input questions with an optional "ground truth" column and outputs a csv with answers. 
-"""
 import pandas as pd
-from docopt import docopt
+import typer
 import chateval.helpers as chat
 
-def main():
-    arguments = docopt(__doc__, version='.00')
-    if arguments['--debug']:
+def main(inputcsv: str,
+         outputcsv: str, 
+         with_context: bool = False, 
+         evaluate: bool = False, 
+         model: str = 'haiku', 
+         debug: bool = False):
+    
+
+    if debug:
         print(arguments)
     
-    with_context = arguments['--with-context']
-
     token = chat.get_token()
 
     print(chat.confirm_login(token))
     # get answers
-    questions = pd.read_csv(arguments['<inputcsv>'])
+    questions = pd.read_csv(inputcsv)
     print("getting answers from the chatbot")
 
     chat.get_answers(questions, token, with_context)
-    if arguments.get('--evaluate'):
-        model = arguments['--model']
-        
+    if evaluate:
         print(f"evaluating answers with {model}")
-        chat.score_answers_df(questions, arguments['--model']).to_csv(arguments['<outputcsv>'], index=False)
+        chat.score_answers_df(questions, model).to_csv(outputcsv, index=False)
     else:
-        questions.to_csv(arguments['<outputcsv>'], index=False) 
+        questions.to_csv(outputcsv, index=False) 
 
-if __name__ == '__main__':
-    main()
+def cli():
+    typer.run(main)
 
